387f5dff50dddf044cfa7d5c71bbe74cd51a7488
Souvik <souviksarkar260123@gmail.com>
2024-10-29 23:05:35 +0530

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView className=" p-4">
+      <Text>index</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
